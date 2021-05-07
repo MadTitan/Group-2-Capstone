33c12a36e1a1bfb66fddefe09c2b46bc9c3a5acf
adelronald <adel.ronald@spcc.edu.ph>
2021-05-07 18:26:47 +0800

    registration

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	include_once ("res/pages/header.php");
+?>
+
+	<h1>Welcome!</h1>
+	<hr>
+	<h4> <a href='registration.php'> Register </a></h4>
+
+<?php
+	include_once ("res/pages/footer.php");
+?>
--- res/php/config/connection.php
+++ res/php/config/connection.php
@@ -3,8 +3,9 @@ ini_set('max_execution_time',0);
 define("host","localhost");
 define("user","root");
 define("pass","");
-define("db","");
-$conn= new mysqli(host,user,pass,db);
+define("db","db_group2");
+
+$conn = new mysqli(host,user,pass,db);
 
 if($conn->connect_error){
 	die("Connection error: ".$conn->connect_error);
