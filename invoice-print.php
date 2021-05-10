49ba736302280b1c6f5a62941904207325c9bd0d
adelronald <adel.ronald@spcc.edu.ph>
2021-05-11 03:57:49 +0800

    sample design

--- invoice-print.php
+++ invoice-print.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Invoice Print</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Bootstrap 4 -->
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body>
-<div class="wrapper">
+<?php
+  include_once ("res/pages/header.php");
+?>
   <!-- Main content -->
   <section class="invoice">
     <!-- title row -->
@@ -160,11 +141,9 @@
     <!-- /.row -->
   </section>
   <!-- /.content -->
-</div>
-<!-- ./wrapper -->
 
-<script type="text/javascript"> 
-  window.addEventListener("load", window.print());
-</script>
-</body>
-</html>
+  
+<?php
+  include_once ("res/pages/footer.php");
+?>
+
