7d6eab118df142d7737c872c094863fbb9cae770
adelronald <adel.ronald@spcc.edu.ph>
2021-05-11 03:30:27 +0800

    admin lte3 implementation

--- index.php
+++ index.php
@@ -2,9 +2,37 @@
 	include_once ("res/pages/header.php");
 ?>
 
-	<h1>Welcome!</h1>
-	<hr>
-	<h4> <a href='registration.php'> Register </a></h4>
+
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Title</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button>
+          </div>
+        </div>
+        <div class="card-body">
+          Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+
 
 <?php
 	include_once ("res/pages/footer.php");
--- res/pages/footer.php
+++ res/pages/footer.php
@@ -1,2 +1,25 @@
+</div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0.1
+    </div>
+    <strong>Copyright &copy; 2021 <a href="https://spcc.edu.ph">SPCC Caloocan</a>.</strong> All rights
+    reserved.
+  </footer>
+
+ 
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
 </body>
 </html>
