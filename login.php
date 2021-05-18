0ae6d6541d8853e24bfb3fbc77164365c5d4a9bb
angeloreyes193 <71683147+angeloreyes193@users.noreply.github.com>
2021-05-18 13:40:00 +0800

    header

--- login.php
+++ login.php
@@ -10,6 +10,26 @@ if(isset($_SESSION['user_id']) || isset($_SESSION['user_access'])){
 }
 
 ?>
+<style type="text/css">
+	#main{
+		background-color: #333;
+		width: 600px;
+		height: 300px;
+		border-radius: 30px;
+
+	}
+	h1{
+		color:white ;
+		background-color: blue;
+		border-top-right-radius: 30px;
+		border-top-left-radius: 30px;
+		size: 300px; 
+		background-image: url('design/download.png');
+
+	}
+</style>
+ <title>SPCC Inventory and Reservation</title>
+ <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
 <body style="background-image: url('assets/BG.png');background-repeat:no-repeat;background-position:center;background-size:;">
 <center>
 <form method="post" action="createsession.php" style="margin-top:10vh;">
@@ -17,7 +37,7 @@ if(isset($_SESSION['user_id']) || isset($_SESSION['user_access'])){
 	<input type="password" name="u_pass" placeholder="Password" maxlength="18" style="text-align:center;" required=""><br>
 	
 	<button name="login_btn">Login</button><br>
-	<hr>
+	
 	<a href="register.php" style="color:black;">Register an Account</a>
 </form>
 </center>
