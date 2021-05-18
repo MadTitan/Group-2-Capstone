be0a671ae845925d6db6bad65120e0462295f9ad
angeloreyes193 <71683147+angeloreyes193@users.noreply.github.com>
2021-05-19 01:50:37 +0800

    navbar

--- register.php
+++ register.php
@@ -3,6 +3,56 @@ session_start();
 require_once "connection.php";
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Admin Page</title>
+ <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
+<body style="background-image: url('design/images/spcc1.jpg');background-repeat:no-repeat;background-position:center;background-size:;">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #bb4e53;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a href="login.php">Home</a>
+  <a class="active" href="#regis.">Register</a>
+
+</div>
+</div>
+
+</body>
+</html>
+
  <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
 <center>
   <title>Admin Page</title>
